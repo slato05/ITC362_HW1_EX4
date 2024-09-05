54fa520b30b1685938b78fb2b3d5c82b71923772
slato05 <slato2001@gmail.com>
2024-09-05 14:35:56 -0500

    Working Version

--- app/src/main/java/com/example/itc362_hw1_ex4/MainActivity.kt
+++ app/src/main/java/com/example/itc362_hw1_ex4/MainActivity.kt
@@ -2,12 +2,37 @@ package com.example.itc362_hw1_ex4
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.itc362_hw1_ex4.databinding.ActivityMainBinding
+
+
+class UserTapCounter {
+    private var tapCount: Int = 0
+    fun addCount() {
+        tapCount++
+    }
+
+    fun getCount(): Int {
+        return tapCount
+    }
+}
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var count = UserTapCounter()
+        val display = binding.TapCounter
+        val btn = binding.TapButton
+
+        btn.setOnClickListener {
+            count!!.addCount()
+
+            display?.setText(count.getCount().toString())
+        }
     }
 }
